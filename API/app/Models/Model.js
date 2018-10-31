21c47ee3e606e059a5b8641e452ca2a0c9af67f7
Enzo García <eramirez@sugit.com.ar>
2018-10-31 13:54:43 -0300

    usuario

--- API/app/Models/Model.js
+++ API/app/Models/Model.js
@@ -5,8 +5,8 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
   host: '93.188.165.3',
   port: 3306,
-  user: 'egandres1997',
-  password: '25531673enzo',
+  user: 'aduser',
+  password: 'asd123',
   database: 'ad_house',
   insecureAuth: true
 })
